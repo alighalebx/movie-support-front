a598edce52b9f623ce309f4e4984ecec94154c5c
AliGhaleb <alighaleb2001@gmail.com>
2023-02-08 20:09:03 +0200

    Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import "./Header.css"
 const Header = () => {
     return <div className="header">
         <div className="container">
-            
+            <div className="logo">
+                <a href="#">Movies</a>
+            </div>
+            <ul className="nav-links">
+
+                <li>
+                    <a>Watch List</a>
+                </li>
+                <li>
+                    <a>Watched</a>
+                </li>
+                <li>
+                    <a className="btn">Add</a>
+                </li>
+                
+            </ul>
 
         </div>
     </div>;
