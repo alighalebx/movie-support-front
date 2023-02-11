2527d9caecdecdba8b598af3a28caa23325f5e42
AliGhaleb <alighaleb2001@gmail.com>
2023-02-11 23:48:45 +0200

    axiosSearchByName

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import { React, useState, useEffect } from 'react';
 import "./Add.css"
+
 const Add = () => {
+  const [searchValue, setSearchValue] = useState("");
+  useEffect(()=>{
+    axios.get(`https://localhost:7059/api/Movie/search by name?name=${searchValue}`)
+    .then((response) => {
+      if(response.data){
+          console.log(response.data);
+      }
+    }).catch((error) => console.log(error));
+  }, [searchValue]);
   return (
     <div className='add-page'>
         
         <div className='container'>
             <div className='add-content'>
                 <div className='input-container'>
-                    <input type="text" placeholder ="HA a movie ? we got every thing you want :)"/>
+                    <input type="text"
+                    placeholder ="HA a movie ? we got every thing you want :)"
+                    value={searchValue.encodeURIComponent}
+                    onChange={(e)=> setSearchValue(e.target.value)}
+                    />
                 </div>
                 <ul className='results'>
                     
